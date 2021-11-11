b76d79303d8bf11af0f1e8fb623065c3774e12a2
marcoweb <marcocarvalho.web@gmail.com>
2021-11-11 17:57:18 +0000

    Criação do Banco de Dados

--- assets/sql/init_data.sql
+++ assets/sql/init_data.sql
@@ -1,1 +1,6 @@
 -- Código SQL com a inclusão dos registros iniciais do banco de dados
+
+INSERT INTO filmes (titulo) VALUES
+    ('Entre Facas e Segredos'),
+    ('A Sociedade do Anel'),
+    ('O Hobbit');
